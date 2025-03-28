9faef7271564ff9235e6ad8865a72ed7c420f3bc
Chinmay Bonde <118678313+CHINMAY214@users.noreply.github.com>
2025-03-28 13:33:54 +0530

    Add files via upload

---
+++ auth0_config.py
@@ -0,0 +1,4 @@
+AUTH0_DOMAIN = "dev-gaqlhvn1k740drks.us.auth0.com"
+CLIENT_ID = "m1RBcUXXRNibVSShTdKIRMnpTx4UDMv3"
+CLIENT_SECRET = "s2cuVBMwRhmTfbtxqSSUd7VfLKq1HNjNmAGagYTqnT78RDgiOj3s0pf_cE5cKJJh"
+REDIRECT_URI = "https://resume-parser-7ywqjnmpk6b9pg3lw243wk.streamlit.app/"  # Change this for deployment
